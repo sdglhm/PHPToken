7a75b6ada9a222d85dee02c83e76b711d37bd998
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2015-01-30 00:36:29 +0530

    Create Token.class.php

---
+++ Token.class.php
@@ -0,0 +1,20 @@
+<?php 
+	/**
+	* This function helps server to identify CSRF
+	*/
+	class Token
+	{
+		public static function generate()
+		{
+			return $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(48));
+		}
+
+		public static function check($token)
+		{
+			if ($token && $_SESSION['token']===$token) {
+				return true;
+			}
+			return false;
+		}
+	}
+ ?>
