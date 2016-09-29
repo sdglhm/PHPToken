4f142c0f31e87d007761c81f2405eb33d77fc1f5
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2016-09-29 17:21:55 +0000

    Applied fixes from StyleCI

--- Session.php
+++ Session.php
@@ -1,9 +1,8 @@
 <?php
+
 //This will help you to start your php session
 //If session isn't started yet, I will start it for you
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
-?>
--- Token.class.php
+++ Token.class.php
@@ -1,21 +1,22 @@
-<?php 
-	/**
-	* This function helps server to identify CSRF
-	*/
-	class Token
-	{
-		public static function generate()
-		{
-			//Generating a random 48 long key
-			return $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(48));
-		}
+<?php
 
-		public static function check($token)
-		{
-			if ($token && $_SESSION['token']===$token) {
-				return true;
-			}
-			return false;
-		}
-	}
- ?>
+    /**
+     * This function helps server to identify CSRF.
+     */
+    class Token
+    {
+        public static function generate()
+        {
+            //Generating a random 48 long key
+            return $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(48));
+        }
+
+        public static function check($token)
+        {
+            if ($token && $_SESSION['token'] === $token) {
+                return true;
+            }
+
+            return false;
+        }
+    }
